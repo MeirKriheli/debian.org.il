340150018ceaf6d6bc15d63017aa675ce57183df
Meir Kriheli <mkriheli@gmail.com>
2017-05-05 00:53:22 +0300

    Initial project

---
+++ config/wsgi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+WSGI config for debian.org.il project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+
+application = get_wsgi_application()
