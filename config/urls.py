0dc31d70f10c23e9e2905ac30fc1f7f01316bbda
Meir Kriheli <mkriheli@gmail.com>
2017-10-11 00:10:57 +0300

    Removed unused import

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,6 @@ debian.org.il URL Configuration
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 
 admin.site.site_header = settings.ADMIN_SITE_HEADER
